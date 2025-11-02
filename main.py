5c902ef72ed083585c6ece4ff9ed87fd2b47b65e
Fidashka <user.name>
2025-11-02 18:46:54 +0400

    a ve b

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ print("Hello world !!!")
 a = ("salam")
 b = ("alaikum")
 print(f"{a}  {b}")
+
+a = 5
+b = 10
+
+if a < b:
+    print(f"{a} is less than {b}")
+else:
+    print(f"{a} is not less than {b}")
+
