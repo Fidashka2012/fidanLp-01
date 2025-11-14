60d649f7f4884111d660ee11af6868a74a66a627
Fidashka <user.name>
2025-11-14 12:24:51 +0400

    tapsiriqlar

--- main.py
+++ main.py
@@ -89,7 +89,94 @@
 
 # print(meyvecikler)
 
-# salam
 
+#task11
+# ededler = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# for eded in ededler:
+#     if eded % 2 == 0:
+#         print(f"Cut eded: {eded}")
 
+# #task12
+# ededler = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# for eded in ededler:
+#     print(eded ** 2)
 
+#task13
+# ededler = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# en_boyuk_eded = ededler[0]
+# for eded in ededler:
+#     if eded > en_boyuk_eded:
+#         en_boyuk_eded = eded
+# print(f"En boyuk eded: {en_boyuk_eded}")
+
+#task14
+
+# ededler = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# en_kicik_eded = ededler[0]
+# for eded in ededler:
+#     if eded < en_kicik_eded:
+#         en_kicik_eded = eded
+# print(f"En kicik eded: {en_kicik_eded}")
+
+# #task15
+
+# list = [10, 20, 30, 40, 50]
+# tersine_list = []
+# for eded in list:
+#         tersine_list.insert(0, eded)
+# print(tersine_list)
+
+# #task16
+
+# ededler = [1, 0, 5, 0, 10]
+# sifirlar = []
+# for eded in ededler:
+#   if eded == 0:
+#     sifirlar.append(eded)
+# print(sifirlar) 
+
+# #task17
+
+# ededler = [1, 2, 3, 4, 5]
+# kvadratlar = []
+# for eded in ededler:
+#     kvadratlar.append(eded ** 2)
+
+# print(kvadratlar)
+
+# #task18
+# eded = [5, 12, 8, 15, 3, 20]
+# cem = 0
+# for sayi in ededler:
+#     if eded > 10:
+#         cem += eded
+# print(cem)
+
+# #task19
+# ededler = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# tek_ededler = []
+# for eded in ededler:
+#     if eded % 2 != 0:
+# #         tek_ededler.append(eded)
+# # print(tek_ededler)
+
+# #task20
+# ededler = [1, 2, 3, 4, 5]
+# yeddi = False
+# for eded in ededler:
+#     if eded == 7:
+#         yeddi = True
+#         break
+# if yeddi:
+#     print("7 ededi siyahida var.")
+# else:
+#     print("7 ededi siyahida yoxdur.")   
+
+# #task21
+# list1 = [1, 2, 3]
+# list2 = [4, 5, 6]
+
+# birlesmis_list = list1 + list2
+# print(birlesmis_list)
+
+# #task22 - helpppp
