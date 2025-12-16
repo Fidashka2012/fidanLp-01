55aca8b20ed081e6416ed2f406f4b025074c8a89
Fidashka <user.name>
2025-12-16 19:53:13 +0400

    mekteb ucundur

--- lazimsiz.py
+++ lazimsiz.py
@@ -5,13 +5,34 @@
 # print(math.sqrt(a))
 
 import turtle
+import colorsys
 t = turtle.Pen()
+s = turtle.Screen().bgcolor("black")
+t.speed(0)
+n = 70
+h = 0
+for i in range (360):
+    c = colorsys.hsv_to_rgb(h, 1, 0.8)
+    h+= 1/n
+    t.color(c)
+    t.left(1)
+    t.fd(1)
+    for j in range (2):
+        t.left (2)
+        t.circle(100)
+
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
 
 
-t.forward(100)
-t.right(90)
-t.forward(100)
-t.right(90)
-t.forward(100)
-t.right(90)
-t.forward(100)
+# for i in range(3):
+#     t.left(120)
+#     t.forward(100)
+#     t.forward(100)
+
+# turtle.done
