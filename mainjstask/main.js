4ebaa401cb3c5d276781f591342876314f405a4d
Fidashka <user.name>
2026-02-05 22:24:21 +0400

    js tasks

---
+++ mainjstask/main.js
@@ -0,0 +1,44 @@
+
+//task1
+let myarr = [1, 2, 3, 4, 5];
+console.log(myarr);
+
+//task2
+let yourarr = [1, 2, 3, 4, 5];
+console.log(yourarr[0]);
+
+//task3
+let arr = [1, 2, 3, 4, 5];
+
+console.log(arr[arr.length - 1]);
+
+//task4
+let massiv = [10, 20, 30, 40];
+massiv[1] = 100;
+console.log(massiv);
+
+//task5
+let thisrarr = [1, 2, 3, 4, 5];
+
+thisrarr.push(6);
+
+console.log(thisrarr);
+
+//task6
+let thisearr = [1, 2, 3, 4, 5];
+
+thisearr.pop();
+
+console.log(thisearr);
+
+//task7 yazammadim
+
+//task8
+let fruits = ["alma", "banan", "armud", "heyva"];
+console.log(fruits.length);
+
+//task9 yazmadim
+
+//task10 yazammadim
+
+
