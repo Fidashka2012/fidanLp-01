8223976d1e9c3782b26afdc9f22201b1168432cd
Fidashka <user.name>
2026-02-19 22:02:11 +0400

    mainjstasks

--- mainjstask/main.js
+++ mainjstask/main.js
@@ -1,42 +1,52 @@
 
-//task1
-let myarr = [1, 2, 3, 4, 5];
-console.log(myarr);
+// //task1
+// let myarr = [1, 2, 3, 4, 5];
+// console.log(myarr);
 
-//task2
-let yourarr = [1, 2, 3, 4, 5];
-console.log(yourarr[0]);
+// //task2
+// let yourarr = [1, 2, 3, 4, 5];
+// console.log(yourarr[0]);
 
-//task3
-let arr = [1, 2, 3, 4, 5];
+// //task3
+// let arr = [1, 2, 3, 4, 5];
 
-console.log(arr[arr.length - 1]);  
+// console.log(arr[arr.length - 1]);  
 
-//task4
-let massiv = [10, 20, 30, 40];
-massiv[1] = 100;
-console.log(massiv);
+// //task4
+// let massiv = [10, 20, 30, 40];
+// massiv[1] = 100;
+// console.log(massiv);
 
-//task5
-let thisrarr = [1, 2, 3, 4, 5];
+// //task5
+// let thisrarr = [1, 2, 3, 4, 5];
 
-thisrarr.push(6);
+// thisrarr.push(6);
 
-console.log(thisrarr);
+// console.log(thisrarr);
 
-//task6
-let thisearr = [1, 2, 3, 4, 5];
+// //task6
+// let thisearr = [1, 2, 3, 4, 5];
 
-thisearr.pop();
+// thisearr.pop();
 
-console.log(thisearr);
+// console.log(thisearr);
 
-//task7 yazammadim
+// //task7 yazammadim
 
-//task8
-let fruits = ["alma", "banan", "armud", "heyva"];
-console.log(fruits.length);
+// //task8
+// let fruits = ["alma", "banan", "armud", "heyva"];
+// console.log(fruits.length);
 
-//task9 yazmadim
+// //task9 yazmadim
 
-//task10 yazammadim
+// //task10 yazammadim
+
+// //task11
+// let menfiolmasin = arr.filter(num => num >= 0);
+// console.log(menfiolmasin);
+//task12
+// let nesemassivdide = [1, 2, 3];
+// let n246elemeliyem = nesemassivdide.map(x => x * 2);
+// console.log(n246elemeliyem);
+
+//task13
