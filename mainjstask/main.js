afe13b673e6a9d13ff44464cbd581a0b6b37699c
Fidashka <user.name>
2026-02-28 19:13:53 +0400

    object tasks

--- mainjstask/main.js
+++ mainjstask/main.js
@@ -42,11 +42,83 @@
 // //task10 yazammadim
 
 // //task11
-// let menfiolmasin = arr.filter(num => num >= 0);
-// console.log(menfiolmasin);
+let menfiolmasin = arr.filter(num => num >= 0);
+console.log(menfiolmasin);
 //task12
-// let nesemassivdide = [1, 2, 3];
-// let n246elemeliyem = nesemassivdide.map(x => x * 2);
-// console.log(n246elemeliyem);
+let nesemassivdide = [1, 2, 3];
+let n246elemeliyem = nesemassivdide.map(x => x * 2);
+console.log(n246elemeliyem);
 
 //task13
+let numbers = [1, 2, 3, 4, 5];
+let result = (num => [num, num * num]);
+console.log(result)
+
+//task14
+const user = {
+  name: "Gulsen",
+  gender: "Qadin",
+  birthday: "01.01.2005"
+};
+
+console.log(user.birthday);
+
+//task15
+
+const user2 = {
+  name: "Gulsen",
+  gender: "Qadin",
+  birthday: "01.01.2005"
+};
+
+user2.age = 20;
+
+delete user2.birthday;
+
+console.log(user2);
+
+//task16
+
+const car = {
+  brand: "BMW",
+  model: "M4",
+  year: 2022
+};
+
+car.color = "Blue";
+
+console.log(car);
+
+//task17
+const user3 = {
+  name: "Gulsen",
+  address: {
+    city: "Baki",
+    country: "Azerbaijan"
+  }
+};
+
+console.log(user3.address.city);
+
+//task18
+const product = {
+  name: "Telefon",
+  price: 150
+};
+
+if (product.price > 100) {
+  console.log("Bahali mehsul");
+} else {
+  console.log("Ucuz mehsul");
+}
+
+//task19
+const book = {
+  title: "4 + 1",
+  author: "Huseyn Nagili"
+};
+
+book.author = "Huseyn Nagili";
+
+console.log(book);
+
